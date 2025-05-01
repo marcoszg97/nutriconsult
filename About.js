5c2262e571214d1ede7a535a004a148e843b1c71
marcoszg97 <marcosgarciazg97@gmail.com>
2025-05-01 12:48:11 -0600

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const About = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Sobre Nosotros</h1>
+      <p>NutriConsult es un sitio profesional para consultas nutricionales en línea.</p>
+    </div>
+  );
+};
+export default About;
---
+++ Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Services = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Servicios</h1>
+      <ul className="list-disc pl-5">
+        <li>Consultas nutricionales personalizadas</li>
+        <li>Planes alimenticios adaptados</li>
+        <li>Control de peso y seguimiento</li>
+      </ul>
+    </div>
+  );
+};
+export default Services;
